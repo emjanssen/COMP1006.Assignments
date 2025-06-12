da9fb464937807fc13a98df898c2f6ef7f4884e1
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-06-12 19:32:03 -0400

    logo added, header/footer edits

--- Assignment1.2025.06.13/js/main.js
+++ Assignment1.2025.06.13/js/main.js
@@ -3,6 +3,13 @@ const footerGlobal = document.querySelector('#footer-global')
 
 headerGlobal.innerHTML = `
     <nav class="header-navigation">
+           
+        <div class="header-logo">
+        <img src="css/images/header_footer_logo.png" alt="Company Logo">
+        </div>
+        
+        <h1 class="company-name">Company Name</h1>
+        
         <ul class="header-navigation-ul">
             <li class="header-navigation-li"><a href="/index.php">Input Hours</a></li>
             <li class="header-navigation-li"><a href="/content.php">Review Content</a></li>
@@ -11,6 +18,12 @@ headerGlobal.innerHTML = `
 
 footerGlobal.innerHTML = `
     <nav class="footer-navigation">
+    
+        <div class="footer-logo">
+        <img src="css/images/header_footer_logo.png" alt="Company Logo">
+
+        </div>
+    
         <ul class="footer-navigation-ul">
             <li class="footer-navigation-li"><a href="/index.php">Input Hours</a></li>
             <li class="footer-navigation-li"><a href="/content.php">Review Content</a></li>
