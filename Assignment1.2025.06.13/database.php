66d6e7e7341b55c770c6926102e7466d500929c4
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-06-13 12:52:44 -0400

    edits to database.php and validate.php
    comments added

--- Assignment1.2025.06.13/database.php
+++ Assignment1.2025.06.13/database.php
@@ -1,24 +1,27 @@
 <?php
 
 // purpose: database connection
-class database
+class Database
 {
     private $host = 'YourHostIP';
     private $user = 'YourUsername';
     private $password = 'YourPassword';
     private $database = 'YourDatabaseName';
     protected $connection;
-
     // protected variable to store our connection
+    // can be accessed in this class and any subclasses
 
     public function __construct()
+        // declare constructor for Database class
     {
         if (!isset($this->connection)) {
             // if our connection hasn't been set yet
             $this->connection = new mysqli($this->host, $this->user, $this->password, $this->database);
-            // create a new connection to the mysql server and assign the parameter names
+            // create a mysql server connection when a Database object is instantiated
+            // passes in values to mysqli parameters and assigns to this instance's $connection property
         } else {
             echo "We are already connected to the database";
+            // if connection already exists, send message to confirm we're connected
         }
     }
 }
--- Assignment1.2025.06.13/validate.php
+++ Assignment1.2025.06.13/validate.php
@@ -1,6 +1,52 @@
 <?php
 // purpose: input validation
-class validate
-{
+class validate {
+    public function checkEmpty($data, $fields) {
+        // create function that takes two arguments
+        $msg = null;
+        // create message variable and assigns value of null
+        foreach ($fields as $value) {
+            // iterate over each element in the $fields array
+            // assigns each array element the name of $value
+            if (empty($data[$value])) {
+                // if the corresponding element in $data (i.e. the data submitted by the user) is empty
+                $msg .= "<p>$value field cannot be empty</p>";
+                // append string and current $value value to $msg
+            }
+        }
+        // return msg; if for each loop did not find empty elements, return message will be null
+        return $msg;
+    }
 
+    public function validAge($age) {
+        // create function that takes one argument
+        if (preg_match("/^[0-9]+$/", $age)) {
+            // use preg_match regex to check if $age contains only the numbers 0-9 inclusive
+            // if pattern matches, return true
+
+            /* syntax:
+            - delimiters to wrap the expression
+            - ^ to indicate start of the string
+            - [0-9] to indicate acceptable values
+            - + to indicate one or more of the preceding element; i.e. 0 - 9 inclusive
+            - $ to indicate the end of the string */
+
+            return true;
+        }
+        // if pattern doesn't match, return false
+        return false;
+    }
+
+    public function validEmail($email) {
+        // create function that takes on argument
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // filter_var() is a built-in function, and FILTER_VALIDATE_EMAIL is a built-in filter
+            // use both to check if value of $email is valid according to standard email formatting rules
+            // if valid, return true
+            return true;
+        }
+        // otherwise, return false
+        return false;
+    }
 }
+?>
