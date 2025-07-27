f782d69c8dfe3318515af8ef377951eb73d9cf92
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-27 05:54:32 -0400

    updated sql; table created in mysql workbench

--- Assignment2.2025.08.10/sql/assignment2.sql
+++ Assignment2.2025.08.10/sql/assignment2.sql
@@ -1,9 +1,12 @@
 CREATE TABLE users_assignment_two
 (
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    username        VARCHAR(50)  NOT NULL UNIQUE,
-    password        VARCHAR(128) NOT NULL,
-    firstName       VARCHAR(128) NOT NULL,
-    lastName        VARCHAR(128) NOT NULL,
-    emailAddress    VARCHAR(128) NOT NULL
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    username      VARCHAR(50)  NOT NULL UNIQUE,
+    password      VARCHAR(128) NOT NULL,
+    first_name    VARCHAR(128) NOT NULL,
+    last_name     VARCHAR(128) NOT NULL,
+    email_address VARCHAR(128) NOT NULL UNIQUE,
+    profile_photo VARCHAR(255) DEFAULT NULL,
+    created_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
