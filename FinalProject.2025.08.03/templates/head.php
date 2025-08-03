447b15d9ee8a07c3568333dbb3ca7b0791355577
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-03 07:04:41 -0400

    initial upload of files/folders for final project

---
+++ FinalProject.2025.08.03/templates/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?php echo htmlspecialchars($pageDescription ?? 'Site description'); ?>">
+    <meta name="keywords" content="<?php echo htmlspecialchars($pageKeywords ?? 'keywords'); ?>">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="../css/img/faviconStar.ico" type="image/x-icon">
+    <title><?php echo htmlspecialchars($pageTitle ?? 'Default Title'); ?></title>
+</head>
+<body>
