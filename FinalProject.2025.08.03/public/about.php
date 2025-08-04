ce1a412b70fadbb5d1ca82c47cdd95cc842a2ed4
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-04 03:07:55 -0400

    added basic structure for each page

--- FinalProject.2025.08.03/public/about.php
+++ FinalProject.2025.08.03/public/about.php
@@ -1,1 +1,22 @@
 <?php
+// start session on page load
+session_start();
+
+// page metadata
+$pageTitle = '';
+$pageDescription = '';
+$pageKeywords = '';
+
+// head and header
+require '../templates/head.php';
+require '../templates/header.php';
+
+// classes
+
+
+/* - - - Functions - - - */
+
+/* - - - Run On Page Load - - - */
+
+// footer
+require '../templates/footer.php';
