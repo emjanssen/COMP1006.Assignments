b26477ae0222bce5c77436c9b18df2f16b75ece5
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-04 03:08:16 -0400

    basic header done

--- FinalProject.2025.08.03/templates/header.php
+++ FinalProject.2025.08.03/templates/header.php
@@ -1,1 +1,25 @@
-<?php
+<header id="global-header">
+    <div id="header-logo">
+        <img id="img-logo" src="../css/img/headerLogo.png" alt="Page logo.">
+    </div>
+
+    <div id="header-title">
+        <h1><?php echo htmlspecialchars($pageTitle ?? 'Default Title'); ?></h1>
+    </div>
+
+    <nav id="header-nav">
+        <ul id="header-nav-ul">
+            <!-- Root Folder -->
+            <li><a href="../index.php">Home</a></li>
+
+            <!-- Public -->
+            <li><a href="../public/about.php">About</a></li>
+            <li><a href="../public/login.php">Login</a></li>
+            <li><a href="../public/register.php">Register</a></li>
+
+            <!-- Private -->
+            <li><a href="../private/profile.php">Profile</a></li>
+            <li><a href="../private/dashboard.php">Dashboard</a></li>
+        </ul>
+    </nav>
+</header>
