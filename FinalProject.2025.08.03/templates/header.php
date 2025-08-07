9dd79b27002d17bb176e5e632681e3dcf6702008
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-07 19:19:40 -0400

    fixed file paths for nav links and img-logo link

--- FinalProject.2025.08.03/templates/header.php
+++ FinalProject.2025.08.03/templates/header.php
@@ -1,6 +1,6 @@
 <header class="global-header">
     <div id="header-logo">
-        <img id="img-logo" src="../css/img/headerLogo.png" alt="Page logo.">
+        <img id="img-logo" src="css/img/headerLogo.png" alt="Page logo.">
     </div>
 
     <div id="header-title">
@@ -9,12 +9,12 @@
 
     <nav id="header-nav">
         <ul id="header-nav-ul">
-            <li><a href="../index.php">Home</a></li>
-            <li><a href="../login.php">Login</a></li>
-            <li><a href="../register.php">Register</a></li>
-            <li><a href="../profile.php">Profile</a></li>
-            <li><a href="../about.php">About</a></li>
-            <li><a href="../dashboard.php">Dashboard</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
         </ul>
     </nav>
 </header>
