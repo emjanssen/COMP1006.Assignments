60ae93bf9a22a1a1e76d16bc33468dbbff954bfb
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-07 19:18:41 -0400

    fixed file paths for css files

--- FinalProject.2025.08.03/templates/head.php
+++ FinalProject.2025.08.03/templates/head.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="<?php echo htmlspecialchars($pageDescription ?? 'Site description'); ?>">
     <meta name="keywords" content="<?php echo htmlspecialchars($pageKeywords ?? 'keywords'); ?>">
-    <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="icon" href="../css/img/faviconStar.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="css/img/faviconStar.ico" type="image/x-icon">
     <title><?php echo htmlspecialchars($pageTitle ?? 'Default Title'); ?></title>
 </head>
 <body>
